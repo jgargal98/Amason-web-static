e7794e5fdf8c7fc69ef857006e372f15b1f2cfd5
juan <juanillogarcia14@gmail.com>
2025-12-17 14:06:39 +0100

    arreglo de errores para el idioma español

--- html/config.php
+++ html/config.php
@@ -4,9 +4,7 @@ $DB_USER = 'if0_40704204';
 $DB_PASS = 'KV0Olnz2QozJHgC';
 $DB_NAME = 'if0_40704204_amason_web';
 
-// Forzar UTF-8 en la conexión
 mysqli_set_charset($conn, "utf8");
 
-// Forzar UTF-8 en el navegador
 header('Content-Type: text/html; charset=utf-8');
 ?>
