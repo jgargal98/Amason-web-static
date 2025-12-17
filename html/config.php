752a6370e63e5b413afbbf8e247072201e3d3738
juan <juanillogarcia14@gmail.com>
2025-12-17 14:08:09 +0100

    arreglo de errores para el idioma español

--- html/config.php
+++ html/config.php
@@ -4,6 +4,11 @@ $DB_USER = 'if0_40704204';
 $DB_PASS = 'KV0Olnz2QozJHgC';
 $DB_NAME = 'if0_40704204_amason_web';
 
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if (!$conn) {
+    die("Error de conexion: " . mysqli_connect_error());
+}
+
 mysqli_set_charset($conn, "utf8");
 
 header('Content-Type: text/html; charset=utf-8');
